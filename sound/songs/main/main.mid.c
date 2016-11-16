afc3026ddac12c43f10eedb572ca5c6fdd7d0902
Jay Garcia <jesusdgarcia@gmail.com>
2016-11-16 14:38:30 -0500

    Added minimal main title track

---
+++ sound/songs/main/main.mid.c
@@ -0,0 +1,9 @@
+// Playtune bytestream for file "main.mid.mid" created by MIDITONES V1.14 on Wed Nov 16 14:35:18 2016
+// command line: miditones main.mid 
+const unsigned char PROGMEM score [] = {
+// Transport
+0x90,48, 0,62, 0x80, 0,62, 0x90,48, 0,62, 0x80, 1,244, 0x90,46, 0,125, 0x80, 0,187, 0x90,48, 0,62, 0x80, 
+0,62, 0x90,48, 0,62, 0x80, 3,44, 0x90,48, 0,62, 0x80, 0,62, 0x90,48, 0,62, 0x80, 1,244, 0x90,36, 0,125, 
+0x80, 0,187, 0x90,48, 0,62, 0x80, 0,62, 0x90,48, 0,62, 0x80, 0xf0};
+// This score contains 69 bytes, and 1 tone generator is used.
+const int PROGMEM numScoreElements = 69;
