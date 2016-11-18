54fe02ed156391e505770b0b6e34fec515f820c9
Simon Prickett <simon@moduscreate.com>
2016-11-17 19:37:57 -0800

    Adds basic dying fx, resets ship pos, prevents shooting or being shot while dying.

--- arduboy-game/player.h
+++ arduboy-game/player.h
@@ -7,18 +7,21 @@
 struct Player {
   byte x;
   byte y;
+  byte dying;
   int frame;
 
   void set() {
     x = 2;
     y = 32;
     frame = 2;
+    dying = 0;
   }
 
   void reset() {
     x = 2;
     y = 32;
     frame = 2;
+    dying = 0;
   }
 };
 
