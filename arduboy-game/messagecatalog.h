5adc1c4811023732668d05eaaf42b98fdc167194
Andy Dennis <andy@byteprinter.com>
2016-11-28 13:41:32 -0500

    Shortened initialis

--- arduboy-game/messagecatalog.h
+++ arduboy-game/messagecatalog.h
@@ -6,17 +6,17 @@
 #define NUM_CREDITS 12
 
 PROGMEM const char credits0[] = "CREDITS";
-PROGMEM const char credits1[] = "Capn: Jay Garcia";
-PROGMEM const char credits2[] = "Medic: Simon Prickett";
-PROGMEM const char credits3[] = "Lt: Stan Bershadskiy"; 
-PROGMEM const char credits4[] = "Helm: Andrew Owen"; 
-PROGMEM const char credits5[] = "Hologram: Andy Dennis"; 
-PROGMEM const char credits6[] = "Gunner: Tim Eagan"; 
-PROGMEM const char credits7[] = "Tech: Drew Griffith"; 
-PROGMEM const char credits8[] = "Loadmaster: JD Jones"; 
-PROGMEM const char credits9[] = "Purser: Jon Van Dalen"; 
-PROGMEM const char credits10[] = "Ngnrng: Lucas Still"; 
-PROGMEM const char credits11[] = "Comms: Matt McCants";
+PROGMEM const char credits1[] = "Capn: J. Garcia";
+PROGMEM const char credits2[] = "Medic: S. Prickett";
+PROGMEM const char credits3[] = "Lt: S. Bershadskiy"; 
+PROGMEM const char credits4[] = "Helm: A. Owen"; 
+PROGMEM const char credits5[] = "Hologram: A. Dennis"; 
+PROGMEM const char credits6[] = "Gunner: T. Eagan"; 
+PROGMEM const char credits7[] = "Tech: D. Griffith"; 
+PROGMEM const char credits8[] = "Loadmaster: JD. Jones"; 
+PROGMEM const char credits9[] = "Purser: J. Van Dalen"; 
+PROGMEM const char credits10[] = "Ngnrng: L. Still"; 
+PROGMEM const char credits11[] = "Comms: M. McCants";
 
 PROGMEM const char * const credits[] = {
   credits0, credits1, credits2, credits3, credits4, credits5, credits6, credits7, credits8, credits9, credits10, credits11
