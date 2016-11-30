06a605d5c24ded0533cceb8bfa94567c9cdaa9d9
Jay Garcia <jesusdgarcia@gmail.com>
2016-11-29 21:19:31 -0500

    fix for https://trello.com/c/U1Hi3eOm/121-bug-enemy-death-before-boss-doesn-t-emit-death-ring-until-after-boss-death

--- arduboy-game/enemy.h
+++ arduboy-game/enemy.h
@@ -48,40 +48,29 @@ struct Enemy {
       difficulty = 1;
       bitmap = enemy1;
       health = 25;
-//      width = 16;
-//      height = 16;
     } else if (type < 9) {
       difficulty = 2;
       bitmap = enemy2;
       health = 150;
-//      width = 16;
-//      height = 16;
     } else if (type == 9) {
-//      difficulty = 4;
       bitmap = enemy3;
       health = 500;
-//      width = 16;
-//      height = 16;
     } else if (type == 128) {
-      difficulty = 4;
       bitmap = boss1;
       health = 1000;
       width = 32;
-//      height = 16;
     } else if (type == 129) {
-      difficulty = 4;
       bitmap = boss2;
       health = 2000;
       width = 32;
-//      height = 16;
     } else if (type == 130) {
-//      difficulty = 4;
       bitmap = boss3;
       health = 3000;
       width = 59;
       height = 53;
     }
 
+    health = 5;
     draw();
   }
 
@@ -98,12 +87,9 @@ struct Enemy {
       if (!isTakingDamage()) {
         draw();
       }
-//    } else if (isDying()) {
     } else if (dying > 0) {
       updateDeathSequence();
-    } else if ((type <= 9)
-      && (!stopSpawningEnemies)
-      && (random(700) == 0)) {
+    } else if ((type <= 9) && (! stopSpawningEnemies) && (random(600) == 0)) {
       spawn();
     }
     
